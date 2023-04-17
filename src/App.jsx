068078ebdb2262c011bd086939e96272aca8aa39
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-04-17 19:22:11 +0530

    React Vite Tailwindcss Starter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl text-center">Hello Welocome To React World</h1>
+    </div>
+  )
+}
+
+export default App
